a66907a48a36cbd7be21f116a7d1f1ae463204b6
Alex Onceanu <alex.onceanu@telecom-paris.fr>
2024-10-06 22:23:05 +0200

    Fini l'invocation de nouvelles figurines

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
-
-@onready var current_scene = $village
 @onready var possessed_figurines = []
 
-
 func _on_collection_pressed() -> void:
-	current_scene.hide()
-	current_scene = $collection
-	current_scene.show()
+	print_debug("pressed")
+	$village.hide()
+	$collection.reset_anim()
+	$collection.show()
 		
 func _on_tower_pressed() -> void:
 	pass
@@ -20,6 +18,8 @@ func _on_tournament_pressed() -> void:
 	pass
 
 func _ready() -> void:
+	$collection.hide()
+	
 	$village/collection.pressed.connect(_on_collection_pressed)
 	$village/tournament.pressed.connect(_on_collection_pressed)
 	$village/tower.pressed.connect(_on_collection_pressed)
